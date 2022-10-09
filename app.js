e8134d1a52c20ba4b4c8179de0324ffd7092b100
fernandobouchet <fernandobouchet@gmail.com>
2022-10-09 15:53:50 -0300

    Added 'compression' and 'helmet' to App.js and update package.json

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
+const compression = require('compression');
+const helmet = require('helmet');
 
 var app = express();
 
@@ -25,6 +27,8 @@ db.on('error', console.log.bind(console, 'MongoDB  conecction error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
